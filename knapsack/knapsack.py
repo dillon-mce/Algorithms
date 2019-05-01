98f09531e33b48df9af7a68eab66c966b39e30e8
Dillon McElhinney <dillon.mce@gmail.com>
2019-05-01 16:50:39 -0600

    Get basic solution to knapsack problem working

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -2,12 +2,42 @@
 
 import sys
 from collections import namedtuple
+from functools import reduce
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  pass
+  values = sorted(items, key = weighted, reverse = True)
+  bag = []
+
+  for item in values:
+    if item.size <= capacity:
+      bag.append(item)
+      capacity -= item.size
   
+  total_value = reduce(sum_value, bag)
+  total_cost = reduce(sum_size, bag)
+  item_names = sorted([x.index for x in bag])
+
+  print(f'Items to select: {item_names}\nTotal cost: {total_cost}\nTotal value: {total_value}')
+
+  return {'Value': total_value, 'Chosen': item_names}
+
+
+def weighted(item):
+    return item.value / item.size
+  
+def sum_value(a, b):
+  if type(a) is type(0):
+    return a + b.value
+  else:
+    return a.value + b.value
+
+def sum_size(a, b):
+  if type(a) is type(0):
+    return a + b.size
+  else:
+    return a.size + b.size
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
